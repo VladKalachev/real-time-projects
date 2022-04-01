17ae3f2b1853b8b420f246c94ea3d8d58a44443c
Vlad <vlvete1990@yandex.ru>
2022-04-01 18:56:30 +0300

    chore: client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LongPulling from './LongPulling';
+
+function App() {
+  return (
+    <div className="App">
+      <LongPulling />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ client/src/LongPulling.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function LongPulling() {
+  return (
+    <div className='center'>
+      <div>
+        <div className='form'>
+          <input type='text' />
+          <button>Отправить</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LongPulling;
