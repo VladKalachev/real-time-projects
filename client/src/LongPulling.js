b65a1522f9a99108b5cf0b6125704fc39dfb246a
Vlad <vlvete1990@yandex.ru>
2022-04-03 22:10:06 +0300

    chore: longpulling

--- client/src/LongPulling.js
+++ client/src/LongPulling.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function LongPulling() {
@@ -6,10 +6,26 @@ function LongPulling() {
   const [value, setValue] = useState('');
 
   const sendMessage = async () => {
-    await axios.post('http://localhost:6000', {
+    await axios.post('http://localhost:4000/new-messages', {
       message: value,
       id: Date.now()
     })
+  }
+
+  useEffect(() => {
+    subscribe();
+  }, [])
+
+  const subscribe = async () => {
+    try {
+      const {data} = await axios.get('http://localhost:4000/get-messages')
+      setMessages(prev => [data, ...prev]);
+      await subscribe();
+    } catch (e) {
+      setTimeout(() => {
+        subscribe();
+      }, 500)
+    }
   }
 
   return (
