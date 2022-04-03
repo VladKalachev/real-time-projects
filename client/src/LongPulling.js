2a36c38fddca7c8c59c14e707cdf07f6b6ae2feb
Vlad <vlvete1990@yandex.ru>
2022-04-03 20:48:55 +0300

    chore: added axios

--- client/src/LongPulling.js
+++ client/src/LongPulling.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function LongPulling() {
+  const [messages, setMessages] = useState([]);
+  const [value, setValue] = useState('');
+
+  const sendMessage = async () => {
+    await axios.post('http://localhost:6000', {
+      message: value,
+      id: Date.now()
+    })
+  }
+
   return (
     <div className='center'>
       <div>
         <div className='form'>
-          <input type='text' />
-          <button>Отправить</button>
+          <input type='text' value={value} onChange={e => setValue(e.target.value)}/>
+          <button onClick={sendMessage}>Отправить</button>
         </div>
+        <div className='messages'>
+          {messages.map(mess =>
+            <div className='message' key={mess.id}>
+              {mess.message}
+            </div>
+          )}
+        </div>
+
       </div>
     </div>
   )
