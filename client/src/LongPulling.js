0b281f4f01c4072da98556dd16aaf586719e0c1e
Vlad <vlvete1990@yandex.ru>
2022-04-11 15:03:47 +0300

    chore: app

--- client/src/LongPulling.js
+++ client/src/LongPulling.js
@@ -15,8 +15,7 @@ function LongPulling() {
   useEffect(() => {
     subscribe();
   }, [])
-  
-  
+
   const subscribe = async () => {
     try {
       const {data} = await axios.get('http://localhost:4000/get-messages')
