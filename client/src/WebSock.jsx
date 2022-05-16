4971b9fe4924d80f311749ae861843020023ea1a
Vlad <vlvete1990@yandex.ru>
2022-05-16 23:19:01 +0300

    chore: websocket

--- client/src/WebSock.jsx
+++ client/src/WebSock.jsx
@@ -1,70 +1,89 @@
-import React, { useRef, useEffect, useState } from 'react';
-import axios from 'axios';
+import React, {useEffect, useRef, useState} from 'react';
+import axios from "axios";
 
-function WebSock () {
-  const [messages, setMessages] = useState([]);
-  const [value, setValue] = useState('');
-  const socket = useRef();
-  const [connected, setConnected] = useState(false);
-  const [userName, setUserName] = useState('');
+const WebSock = () => {
+    const [messages, setMessages] = useState([]);
+    const [value, setValue] = useState('');
+    const socket = useRef()
+    const [connected, setConnected] = useState(false);
+    const [username, setUsername] = useState('')
 
-  useEffect(() => {
-   socket.current = new WebSocket('ws://localhost:6000')
+    function connect() {
+        socket.current = new WebSocket('ws://localhost:8080')
 
-   socket.current.onopne = () => {
-    setConnected(true);
-   }
-   socket.current.onmessage = () => {
-       
-   }
-   socket.current.onclose = () => {
-      console.log('Socket закрыт')
-   }
-   socket.current.onerror = () => {
-      console.log('Socket произошла ошибка')
-   }
-  }, [])
+        socket.current.onopen = () => {
+            setConnected(true)
+            const message = {
+                event: 'connection',
+                username,
+                id: Date.now()
+            }
+            socket.current.send(JSON.stringify(message))
+        }
+        socket.current.onmessage = (event) => {
+            const message = JSON.parse(event.data)
+            setMessages(prev => [message, ...prev])
+        }
+        socket.current.onclose= () => {
+            console.log('Socket закрыт')
+        }
+        socket.current.onerror = () => {
+            console.log('Socket произошла ошибка')
+        }
+    }
 
-  const sendMessage = async () => {
-    await axios.post('http://localhost:4000/new-messages', {
-      message: value,
-      id: Date.now()
-    })
-  }
+    const sendMessage = async () => {
+        const message = {
+            username,
+            message: value,
+            id: Date.now(),
+            event: 'message'
+        }
+        socket.current.send(JSON.stringify(message));
+        setValue('')
+    }
 
-  if(!connected) {
-    return (
-      <div className='center'>
-        <div className='form'>
-          <input 
-            value={userName} 
-            onChange={e => setUserName(e.target.value)} 
-            type="text" 
-            placeholder='Введите имя пользователя' />
-          <button>Войти</button>
-        </div>
-      </div>
-    )
-  }
 
-  return (
-    <div className='center'>
-      <div>
-        <div className='form'>
-          <input type='text' value={value} onChange={e => setValue(e.target.value)}/>
-          <button onClick={sendMessage}>Отправить</button>
-        </div>
-        <div className='messages'>
-          {messages.map(mess =>
-            <div className='message' key={mess.id}>
-              {mess.message}
+    if (!connected) {
+        return (
+            <div className="center">
+                <div className="form">
+                    <input
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                        type="text"
+                        placeholder="Введите ваше имя"/>
+                    <button onClick={connect}>Войти</button>
+                </div>
             </div>
-          )}
-        </div>
+        )
+    }
 
-      </div>
-    </div>
-  )
-}
+
+    return (
+        <div className="center">
+            <div>
+                <div className="form">
+                    <input value={value} onChange={e => setValue(e.target.value)} type="text"/>
+                    <button onClick={sendMessage}>Отправить</button>
+                </div>
+                <div className="messages">
+                    {messages.map(mess =>
+                        <div key={mess.id}>
+                            {mess.event === 'connection'
+                                ? <div className="connection_message">
+                                    Пользователь {mess.username} подключился
+                                </div>
+                                : <div className="message">
+                                    {mess.username}. {mess.message}
+                                </div>
+                            }
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default WebSock;
--- server/websocket.js
+++ server/websocket.js
@@ -1,18 +1,18 @@
 const ws = require('ws');
 
 const wss = new ws.Server({
-    port: 6000,
-}, () => console.log('Server started on 6000'));
+    port: 8080,
+}, () => console.log(`Server started on 8080`))
 
-wss.on('connection', function(ws) {
-    ws.on('message', function(message) {
-        message = JSON.parse(message);
+wss.on('connection', function connection(ws) {
+    ws.on('message', function (message) {
+        message = JSON.parse(message)
         switch (message.event) {
             case 'message':
-                broadcastMessage(message);
+                broadcastMessage(message)
                 break;
             case 'connection':
-                broadcastMessage(message);
+                broadcastMessage(message)
                 break;
         }
     })
